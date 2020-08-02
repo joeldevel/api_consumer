dcb5a3bf45d6169e6ebfafe8d531042d37d2a855
joeldevel <christianjoel.developer@gmail.com>
2020-08-01 23:40:59 -0300

    data can be retrieved

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+
 import Header from './components/Header';
 
+import './App.css';
+
 function App()  {
+  // Use the hooks
+  const [ items, setItems ] = useState([]);
+  const [ isLoading, setIsLoading ] = useState(true);
+
+  useEffect(()=>{
+    async function fetchData() {
+      const fetchItems = await fetch('https://breakingbadapi.com/api/characters');
+      const result = await fetchItems.json();
+      console.log( result );
+    }
+    fetchData();
+  } , [])
   return (
     <div className="container">
       <Header />
