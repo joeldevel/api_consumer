4b67522d007f7dd5cd36e956d827bd089c5615ad
joeldevel <christianjoel.developer@gmail.com>
2020-08-01 23:26:56 -0300

    Header component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App()  {
   return (
     <div className="container">
-    hola
+      <Header />
     </div>
   );
 }
